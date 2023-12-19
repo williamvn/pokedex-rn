ae6d7844b170563ad721a402826d36586bae503c
williamvn <wnoguerol@commercehub.com>
2023-12-19 12:00:37 +0100

    Set colors to cards

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import { PokeInfo } from '../types/PokemonApiResponse';
+import { default as colors} from "../assets/colors.json";
 
 interface PokeListProps {
     pokemon: PokeInfo;
 }
 
+
 export const PokeCard = ({ pokemon }: PokeListProps) => {
+    const [bgColor, setBgColor] = useState('gray')
+
+    useEffect(() => {
+        // This is work around to set some colors to the card since the tool to extract colors from pokemons is not working
+        const rndIndex = Math.floor((Math.random() * colors.length)); 
+        setBgColor(colors[rndIndex])
+    }, [pokemon]);
+
+
     return (
-        <View style={styles.card}>
-            <Image source={{ uri: pokemon.picture }} style={{ width: 150, height: 150 }} />
-            <Text style={styles.pokemonTitle}>{pokemon.name}</Text>
+        <View style={{ ...styles.card, backgroundColor: bgColor }}>
+            <Image source={{ uri: pokemon.picture }} style={styles.pokeImage} />
+            <Text style={styles.pokemonTitle}>{pokemon.name}{'\n#' + pokemon.id}</Text>
+            <View style={styles.pokeBallWrapper}>
+                <Image source={require("../assets/white-pokeball.png")} style={styles.pokeBall} />
+            </View>
         </View>
     )
 }
@@ -18,18 +32,48 @@ const styles = StyleSheet.create({
     card: {
         display: 'flex',
         flexDirection: 'row',
+        backgroundColor: "gray",
+        justifyContent: 'flex-end',
+        padding: 10,
+        margin: 10,
+        marginHorizontal: 13,
+        borderRadius: 10,
+        height: 150
+    },
+    pokeImage: {
+        width: 170,
+        height: 170,
+        position: 'absolute',
+        left: -20,
+        bottom: -20,
+        zIndex: 1
+    },
+    pokeBallWrapper: {
+        width: 200,
+        height: 150,
+        right: 0,
+        bottom: 0,
+        position: 'absolute',
+        overflow: 'hidden'
+    },
+    pokeBall: {
+        width: 200,
+        height: 200,
+        position: 'absolute',
+        right: -30,
+        bottom: -30,
+        opacity: 0.3
     },
     pokemonTitle: {
         textTransform: 'uppercase',
-        fontSize: 32, // Adjust the font size as needed
+        fontSize: 32,
         fontWeight: 'bold',
         fontStyle: 'italic',
-        letterSpacing: 0, // Adjust the letter spacing as needed
-        color: '#FF4500', // Choose a color that suits your design
-        textAlign: 'center', // Align text in the center
-        //   transform: [{ rotate: '-10deg' }], // Apply inclination
-        marginBottom: 10, // Adjust spacing as needed
+        letterSpacing: 0,
+        color: 'white',
+        opacity: 0.9,
+        textAlign: 'right', 
+        marginBottom: 10, 
         marginLeft: 20,
-        alignSelf: 'center'
     },
 });
