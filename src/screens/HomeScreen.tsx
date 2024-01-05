0330060167dd23d223bdf6007a36cdb25df14dd8
williamvn <wnoguerol@commercehub.com>
2024-01-05 15:55:08 +0100

    Fadeout header

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Animated, Easing, FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../theme/AppTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { usePokemons } from '../hooks/usePokemons';
 import { PokeCard } from '../components/PokeCard';
+import { useAnimation } from '../hooks/useAnimation';
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
   const { pokemons, loadPokemons, isLoading } = usePokemons();
-
+  const { value, animate } = useAnimation(1, Easing.ease);
   return (
     <View style={{ flex: 1, paddingTop: top }}>
       <Image source={require("../assets/pokeball.png")} style={styles.image} />
       <View style={globalStyles.container}>
         <FlatList
-          ListHeaderComponent={<Text style={{ ...globalStyles.textHeader}}>Pokedex</Text>}
+          ListHeaderComponent={<Animated.View style={{ opacity: value }}><Text style={{ ...globalStyles.textHeader, marginTop: 10}}>Pokedex</Text></Animated.View>}
           data={pokemons}
           renderItem={({ item }) => <PokeCard pokemon={item} />}
           onEndReached={loadPokemons}
+          onScroll={(y) => animate(1 - y.nativeEvent.contentOffset.y / 30, 10)}
           onEndReachedThreshold={0.5}
           ListFooterComponent={<View style={{ height: 130 }}>
             {isLoading && <ActivityIndicator size={25} color={"grey"} />}
